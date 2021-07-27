76f83f3ee76c07f61247dd4ed88f84e00ec4528a
Ola <olka55255@gmail.com>
2021-07-27 23:43:08 +0200

    save and read from file

--- hamming.srcs/sim_1/new/decoder_rtl_tb.v
+++ hamming.srcs/sim_1/new/decoder_rtl_tb.v
@@ -26,30 +26,42 @@ reg rst;
 reg [25:0] data_out;
 reg [31:0] data_in;
 reg [1:0] status;
-reg unsigned [31:0] test_data[0:2] = { 32'b11011010011110100100101011111111,32'b11011010011110100100101010110011,32'b01011010011101001010001110111110};
-reg unsigned [25:0] out_expected[0:2] = {26'b11011010011110101001011111,26'b11011010011110101001011010, 26'b01011010011101010100011011 };
-reg unsigned [1:0] status_expected[0:2] = {2'b01,2'b10,2'b00};   
+reg unsigned [31:0] test_data;//[0:2] = { 32'b11011010011110100100101011111111,32'b11011010011110100100101010110011,32'b01011010011101001010001110111110};
+reg unsigned [25:0] out_expected;//[0:2] = {26'b11011010011110101001011111,26'b11011010011110101001011010, 26'b01011010011101010100011011 };
+reg unsigned [1:0] status_expected;//[0:2] = {2'b01,2'b10,2'b00};   
+ 
+integer file, out_file, data;
+integer i=0;
  
 decoder_rtl e1 (.clk(clk), .hamming_in(data_in), .rst(rst), .data_out(data_out), .status_out(status));
 
 clk c1 (.clk(clk));
 
-integer i;
-
 initial begin
     rst=1;
     #10
     rst=0;
-    for (i=0;i<3;i=i+1) begin
-        data_in = test_data[i];
-        #20
-        if(data_out==out_expected[i]&& status==status_expected[i])
-            $display("Test %d ok, status %b", i, status);
-        else
-            $display("Test %d fail %b, expected %b, status %b", i, data_out, out_expected[i], status);
-        
-    end
     
+    file = $fopen("D:/ham/decoder.TXT", "r");
+    out_file = $fopen("D:/ham/decoder_out.TXT", "w");
+    
+    while(!$feof(file)) 
+        begin
+            data =  $fscanf(file, "%b %b %b\n", test_data, out_expected, status_expected);
+            data_in = test_data;
+            #20
+            if(data_out==out_expected && status==status_expected)
+                $fwrite(out_file, "Test %0d ok, status %b\n", i, status);
+                //$display("Test %d ok, status %b", i, status);
+            else
+                $fwrite(out_file, "Test %0d fail %b, expected %b, status %b\n",i,data_out, out_expected, status);
+                //$display("Test %d fail %b, expected %b, status %b", i, data_out, out_expected, status);
+            i=i+1;
+            end
+            
+    $fclose(file);
+    $fclose(out_file);
+
     #10 $display("Finished ");
     $stop;
 
--- hamming.srcs/sim_1/new/encoder_rtl_tb.v
+++ hamming.srcs/sim_1/new/encoder_rtl_tb.v
@@ -25,26 +25,39 @@ wire clk;
 reg rst;
 reg [25:0] data_in;
 reg [31:0] data_out;
-reg unsigned [25:0] test_data[0:2] = { 26'b11111010011101010100011011,26'b11011010011110110100011011,26'b11011010011110101001011011};
-reg unsigned [31:0] out_expected[0:2] = {32'b11111010011101001010001110111011,32'b11011010011110101010001010101000,32'b11011010011110100100101010111111 };
+reg unsigned [25:0] test_data;//[0:2] = { 26'b11111010011101010100011011,26'b11011010011110110100011011,26'b11011010011110101001011011};
+reg unsigned [31:0] out_expected;//[0:2] = {32'b11111010011101001010001110111011,32'b11011010011110101010001010101000,32'b11011010011110100100101010111111 };
     
 encoder_rtl e1 (.clk(clk), .data_in(data_in), .rst(rst), .data_out(data_out));
 clk c1 (.clk(clk));
-integer i;
+
+integer i=0;
+integer file, out_file, data;
 
 initial begin
     rst=1;
     #10
     rst=0;
-    for (i=0;i<3;i=i+1) begin
-        data_in = test_data[i];
-        #20
-        if(data_out==out_expected[i])
-            $display("Test %d ok", i);
-        else
-            $display("Test %d fail %b, expected %b", i, data_out, out_expected[i]);
-        
-    end
+    
+    file = $fopen("D:/ham/encoder.TXT", "r");
+    out_file = $fopen("D:/ham/encoder_out.TXT", "w");
+    
+        while(!$feof(file)) 
+        begin
+            data =  $fscanf(file, "%b %b %b\n", test_data, out_expected);
+            data_in = test_data;
+            #20
+            if(data_out==out_expected)
+                $fwrite(out_file, "Test %0d ok\n", i);
+                //$display("Test %d ok, status %b", i, status);
+            else
+                $fwrite(out_file, "Test %d fail %b, expected %b", i, data_out, out_expected);
+                //$display("Test %d fail %b, expected %b, status %b", i, data_out, out_expected, status);
+            i=i+1;
+            end
+            
+    $fclose(file);
+    $fclose(out_file);
     
     #10 $display("%0t: Finished TPG.", $time);
     $stop;
